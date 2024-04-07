273a7772b96c05754b0989a7d9f572798f680a68
Techin Supapol <techin.sup@ascendcorp.com>
2024-04-07 20:04:20 +0700

    fix: font name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,64 +1,24 @@
-import type { Metadata } from "next";
-import localFont from 'next/font/local';
+import type {Metadata} from "next"
 
-import "./globals.css";
-
-export const chulaCharas = localFont({
-  src: [
-    {
-      path: '../../public/fonts/ChulaCharasNew-Bold.woff2',
-      weight: 'bold',
-      style: 'normal',
-    },
-    {
-      path:  '../../public/fonts/ChulaCharasNew.woff2',
-      weight: 'normal',
-      style: 'normal',
-    },
-    {
-      path:  '../../public/fonts/ChulaCharasNew-Italic.woff2',
-      weight: 'normal',
-      style: 'italic',
-    },
-    {
-      path:  '../../public/fonts/ChulaCharasNew-BoldItalic.woff2',
-      weight: 'bold',
-      style: 'italic',
-    }
-  ],
-  variable: '--font-chula-charas'
-});
-
-export const chulalongkorn = localFont({
-  src: [
-    {
-      path: '../../public/fonts/Chulalongkorn-Regular.woff2',
-      weight: 'normal',
-      style: 'normal',
-    },
-    {
-      path:  '../../public/fonts/Chulalongkorn-Bold.woff2',
-      weight: 'bold',
-      style: 'normal',
-    },
-  ],
-  variable: '--font-chulalongkorn'
-});
-
+import "./globals.css"
+import {chulaCharas, chulalongkorn} from "@/styles/fonts/fonts"
 
 export const metadata: Metadata = {
   title: "Pawsome Match",
   description: "Pawsome Match",
-};
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
-      <body className={`${chulaCharas.variable} ${chulalongkorn.variable} font-chulacharas`}>{children}</body>
+      <body
+        className={`${chulaCharas.variable} ${chulalongkorn.variable} font-chulacharas`}>
+        {children}
+      </body>
     </html>
-  );
+  )
 }
