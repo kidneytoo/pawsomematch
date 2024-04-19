ad4269f232517b11e220da1afe7366264265913a
Techin Supapol <techin.sup@ascendcorp.com>
2024-04-19 12:50:12 +0700

    feat: fix middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,9 +4,7 @@ export function middleware(request: Request) {
   const nonce = Buffer.from(crypto.randomUUID()).toString("base64");
   const cspHeader = `
     default-src 'self';
-    script-src 'self' 'nonce-${nonce}' 'strict-dynamic' https: http: 'unsafe-inline' ${
-    process.env.NODE_ENV === "production" ? "" : `'unsafe-eval'`
-  };
+    script-src 'self' 'nonce-${nonce}' 'strict-dynamic' https: http: 'unsafe-inline' 'unsafe-eval';
     style-src 'self' 'unsafe-inline';
     img-src 'self' blob: data:;
     font-src 'self';
